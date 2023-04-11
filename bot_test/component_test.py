b304f80266b62ac62bc05a552694862dcf3efdd8
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2023-04-11 20:39:13 +0900

    component test(also not complite)

--- bot_test/component_test.py
+++ bot_test/component_test.py
@@ -4,6 +4,7 @@ from discord.http import Route
 app = discord.Client(intents=discord.Intents.all())
 http = app.http
 
+button_clicked = {}
         
 buttons = [
     {
@@ -48,10 +49,57 @@ async def on_message(msg: discord.Message):
         
         r = Route('POST', '/channels/{channel_id}/messages', channel_id=msg.channel.id)
         payload = {
+                'embed': embed.to_dict(),
                 'components': buttons
         }
-        await msg.channel.send(embed=embed)
-        await http.request(r, json=payload)
+        response = await http.request(r, json=payload)
+        
+        button_clicked[response.get('id')] = {
+            'button_1': 0, 'button_2': 0, 'button_3': 0, 'button_4': 0
+        }
         return
+
+@app.event
+async def on_socket_response(msg: dict):
+    d = msg.get('d', {})
+    t = msg.get('t');
+    if t == 'INTERACTION_CREATE' and d.get('type') == 3:
+        message = d.get('message', {})
+        custom_id = d.get('data', {}).get('custom_id')
+        
+        button_clicked[message.get('id', 0)][custom_id] += 1
+        
+        embed = discord.Embed(
+                title='Buttons',
+                description='''Button 1 : {} clicked
+                            Button 2 : {} clicked
+                            Button 3 : {} clicked
+                            Button 4 : {} clicked'''.format(
+                                button_clicked[message.get('id', 0)]['button_1'], 
+                                button_clicked[message.get('id', 0)]['button_2'], 
+                                button_clicked[message.get('id', 0)]['button_3'], 
+                                button_clicked[message.get('id', 0)]['button_4']),
+                colour=0x0080ff
+        )
+        
+        r = Route('PATCH', '/channels/{channel_id}/messages/{message_id}', channel_id=msg.message.get('channel_id').id, message_id=message.get('id'))
+        payload = {
+            'embed': embed.to_dict(),
+            'components': buttons
+        }
+        await http.request(r, json=payload)
+        
+        interaction_id = d.get("id")
+        interaction_token = d.get("token")
+
+        await app.http.request(
+            Route("POST", f"/interactions/{interaction_id}/{interaction_token}/callback"),
+            json={"type": 4, "data": {
+                "content": "당신은 {}를 고르셨군요!".format(custom_id),
+                "flags": 64
+            }},
+        )
+        
+    return
 
 app.run('my tokken')
